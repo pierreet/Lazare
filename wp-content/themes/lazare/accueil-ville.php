6e99cd9c6cefb2383dd6f62f07a5de86c917b786
Pierre Etienne Crepy <pierre-etienne@crepy.eu>
2013-06-06 20:57:52 +0200

    creation fichier accueil-ville.php

---
+++ wp-content/themes/lazare/accueil-ville.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ *
+ * Template Name: accueil-ville
+ */
+ 
+ get_header(); // Inclut votre header
+ global $post;
+ 
+ $page_id = $post->ID;
+?>
+<div id="primary">
+	<div role="main" id="content">		
+		<div class="colonnes">
+		<?php $page_data = get_page( $page_id ); ?> 
+		<?php echo apply_filters('the_content', $page_data->post_content);?>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
