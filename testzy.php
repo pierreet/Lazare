6933b401b1a1652b3aafb6234bd5ed3d63179c8e
Charles Dupoiron <charlesdupoiron@gmail.com>
2014-01-06 17:37:09 +0100

    Correction reçus fiscaux

--- testzy.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo(" ");
-$id = 118;
-$location = "http://www.maisonlazare.com/wp-admin/admin.php?page=wplazare_m_orders&action=deleteok&saveditem=".$id;
-$status = "302";
-header("Location: $location", true, $status);
